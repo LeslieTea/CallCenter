aefd1a9d5f332397478c39ed9c5ab136e740a116
LeslieTea <a.leslieann@gmail.com>
2015-03-20 03:40:39 +0000

    login/register surface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 PostitTemplate::Application.routes.draw do
   root to: 'welcome#index'
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   
   resources :scripts do
+  end
+  
+  resources :users, only: [:show, :create, :edit, :update] do
   end
 end
